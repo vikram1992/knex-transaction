8841bd711713b09a2774eb67e69e47b1e2699a7d
vikram <vikram@whitehatjr.com>
2019-08-29 16:24:28 +0530

    Task:Base Model

--- models/BaseModel.js
+++ models/BaseModel.js
@@ -1,7 +1,7 @@
 const { Model } = require("objection");
 const cls = require("cls-hooked");
 
-module.export = class BaseModel extends Model {
+module.exports = class BaseModel extends Model {
   static query(...args) {
     const query = super
       .query(...args)
